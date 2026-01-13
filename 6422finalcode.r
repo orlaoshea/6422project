105eb01bf1a5434aa7ad94125d4fe729b312addc
Orla O'Shea <orlaviolet@Orlas-MacBook-Air.local>
2026-01-13 12:13:33 +0000

    Save local changes before rebase

--- 6422finalcode.r
+++ 6422finalcode.r
@@ -1,18 +1,7 @@
----
-title: "Orla O'Shea PSY6422 Project"
-format: html
-editor: visual
----
-
-# Project Script
-
-Below is the final script with relevant narrative that produces the heatmap visualisation:
-
-```{r}
 library(dplyr) 
 library(tidyr) #Both packages allowed me to clean the data before visualising it.
 
-#I had to remove the first few headings to make sure the data read correctly,
+#I had to remove the first few headings to make sure the data read correctly, 
 #and then separated the variable columns from the data as they had merged together.
 df <- read.csv("gcses-and-equivalent-in-english-and-maths-table-data.csv", 
                skip = 7, 
@@ -40,6 +29,32 @@ df_long_clean <- df_long %>%
   mutate(Value = as.numeric(Value)) %>% 
   filter(!is.na(Value))
 
+#Summary of cleaned data.
+summary(df_long_clean)
+#Area code          Area name             Year               Value      
+#Length:2642        Length:2642        Length:2642        Min.   :50.13  
+#Class :character   Class :character   Class :character   1st Qu.:69.56  
+#Mode  :character   Mode  :character   Mode  :character   Median :73.73  
+                                                          #Mean   :73.62  
+                                                          #3rd Qu.:78.07  
+                                                          #Max.   :90.68
+
+#Showing the first few rows of data as a tibble.
+head(df_long_clean, 10)
+# A tibble: 10 × 4
+#`Area code` `Area name`   Year    Value
+#<chr>       <chr>         <chr>   <dbl>
+#1 E06000001   Hartlepool    2016-17  74.2
+#2 E06000001   Hartlepool    2017-18  69.4
+#3 E06000001   Hartlepool    2018-19  66.0
+#4 E06000001   Hartlepool    2019-20  67.6
+#5 E06000001   Hartlepool    2020-21  70.5
+#6 E06000001   Hartlepool    2021-22  69.6
+#7 E06000001   Hartlepool    2022-23  72.8
+#8 E06000001   Hartlepool    2023-24  74.0
+#9 E06000002   Middlesbrough 2016-17  63.2
+#10 E06000002   Middlesbrough 2017-18  64.3
+
 library(ggplot2)
 library(tidyverse)
 library(plotly) #These packages allowed me to visualise the data and make it interactive.
@@ -57,7 +72,6 @@ df_heatmap <- df_long_clean %>%
   labs(title = "Attainment of GCSEs in English and Maths across England",
        x = "Year", y = "Lower-tier/Unitary Authority", fill = "Pass Rate") +
   theme(axis.text.y = element_text(size = 4))
-
 df_heatmap
 
 df_heatmap +
@@ -73,4 +87,3 @@ df_heatmap +
 #and used the plotly package to make the map interactive so you can get the specific
 #percentages of qualifications for each region per academic year.
 ggplotly(df_heatmap, height = 1600)
-```
